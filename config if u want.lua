fa9c69e917079e871f1378b962139bb905fae3d2
w111user <109961811+w111user@users.noreply.github.com>
2024-11-06 22:56:37 +0700

    Config here! (its farm level so long so buy the real one :} )

---
+++ config if u want.lua
@@ -0,0 +1,70 @@
+--config for that hub (switch hub sources i got it from discord if u ask why i have the script) if u want: 
+-- Quest 
+_G.Quest = { 
+    ['RGB Aura Haki'] = true,
+    ['Pull Lever'] = true,
+    ['Quest Dough Awaken'] = true
+}
+
+-- Race 
+_G.Race = { 
+    ['Select Race'] = {'Human','Skypiea','Mink'},
+    ['Lock Race'] = true,
+    ['Evo Race V3'] = true
+}
+
+-- Melee 
+_G.Melee = { 
+    ['Godhuman'] = true
+}
+
+-- Sword
+_G.Sword = { 
+    ['Saber'] = true,
+    ['Midnight Blade'] = true,
+    ['Shisui'] = true,
+    ['Saddi'] = true,
+    ['Wando'] = true,
+    ['Yama'] = true,
+    ['Koko'] = false,
+    ['Rengoku'] = true,
+    ['Canvander'] = true,
+    ['Buddy Sword'] = true,
+    ['Twin Hooks'] = true,
+    ['SpikeyTrident'] = true,
+    ['Hallow Scryte'] = true,
+    ['Dark Dagger'] = true,
+    ['Tushita'] = true,
+    ['True Triple Katana'] = true,
+    ['Cursed Dual Katana'] = true,
+    ['Shark Anchor'] = true
+}
+
+-- Farm Gun 
+_G.Gun = {  
+    ['Kabucha'] = true,
+    ['Acidum Rifle'] = true,
+    ['Soul Guitar'] = true, 
+    ['Serpent Bow'] = true
+}
+
+-- Devil Fruit 
+_G.Fruit = { 
+    ['Main Fruit'] = {'Kitsune-Kitsune'},
+    ['Select Fruit'] = {'Dragon-Dragon'}
+}
+
+-- Mastery 
+_G.Mastery = { 
+    ['Melee'] = true,
+    ['Sword'] = true,
+    ['Fruit'] = true
+}
+
+-- Setting 
+_G.Setting = {
+    ['FPS Booster'] = true,
+    ['Close Ui'] = true 
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/w111user/randomscript/refs/heads/main/randomkaitunforskid.lua"))()
